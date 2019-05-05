86957b28600551f18522585697b8cd1df4330a4a
Vladimir Zeifman <edoomik@gmail.com>
2019-05-05 13:07:34 +0300

    Bypass the API CORS issue

--- src/services/request.js
+++ src/services/request.js
@@ -2,7 +2,7 @@ import CONFIG from '../config';
 
 
 export default function request(url, options) {
-   const urlWithApiKey = `${url}&apikey=${CONFIG.API_KEY}&format=json`;
+   const urlWithApiKey = `${url}&apikey=${CONFIG.API_KEY}&format=jsonp&callback=callback`;
    const optionsWithDefaults = {...options, headers: {"Content-Type": "application/json"}, mode: "cors"};
 
    return fetch(urlWithApiKey, optionsWithDefaults).then((response) => response.json());
